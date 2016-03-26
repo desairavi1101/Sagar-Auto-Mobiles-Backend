cb47c50c2e02295589c1eb2ddd85dccd434b0309
desairavi1101 <desai.ravi1101@gmail.com>
2016-03-26 22:51:56 +0530

    Added Connection string and DBHelper

--- Sagar-Auto-Mobiles-API/Sagar-Auto-Mobiles-API/App_Start/WebApiConfig.cs
+++ Sagar-Auto-Mobiles-API/Sagar-Auto-Mobiles-API/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace Sagar_Auto_Mobiles_API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "SagarAutoPartsApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
---
+++ Sagar-Auto-Mobiles-API/Sagar-Auto-Mobiles-API/Utility/DBHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Sagar_Auto_Mobiles_API.Utility
+{
+    public class DBHelper
+    {
+        private static MySqlConnection _connection;
+
+        public static MySqlConnection Connection
+        {
+            get
+            {
+                if (_connection == null)
+                {
+                    _connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["SagarAutoParts"].ConnectionString);
+                }
+                return _connection;
+            }
+        }
+        
+        public static DataTable ExecuteSelect(string selectQuery)
+        {
+            MySqlCommand cmd = new MySqlCommand(selectQuery,Connection);
+            if (Connection.State != ConnectionState.Open)
+            {
+                Connection.Open();
+            }
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            if (Connection.State != ConnectionState.Closed)
+            {
+                Connection.Close();
+            }
+
+            return ds.Tables[0];
+
+        }
+
+
+    }
+}
