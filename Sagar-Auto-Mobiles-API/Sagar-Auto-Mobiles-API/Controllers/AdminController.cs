33006cb4c2bc7a52fd8238b93555ad4cc029c283
desairavi1101 <desai.ravi1101@gmail.com>
2016-03-26 22:52:27 +0530

    Added Model and Controller for Admin

---
+++ Sagar-Auto-Mobiles-API/Sagar-Auto-Mobiles-API/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sagar_Auto_Mobiles_API.Controllers
+{
+    /// <summary>
+    /// Controller for Admin
+    /// </summary>
+    public class AdminController : ApiController
+    {
+        [ActionName("Login")]
+        [HttpPost]  
+        public IHttpActionResult Login()
+        {
+            return null;
+        }
+    }
+}
---
+++ Sagar-Auto-Mobiles-API/Sagar-Auto-Mobiles-API/Models/Admin.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Sagar_Auto_Mobiles_API.Models
+{
+    /// <summary>
+    /// Model for Admin
+    /// </summary>
+    public class Admin
+    {
+        private string _email;
+
+        /// <summary>
+        /// Email Address of Admin
+        /// </summary>
+        [JsonProperty(PropertyName="email")]
+        public string Email
+        {
+            get
+            {
+                return this._email;
+            }
+            set
+            {
+                this._email = value;
+            }
+            
+        }
+
+        private string _firstName;
+
+        /// <summary>
+        /// First name of Admin
+        /// </summary>
+        [JsonProperty(PropertyName = "firstname")]
+        public string FirstName
+        {
+            get
+            {
+                return this._firstName;
+            }
+            set
+            {
+                this._firstName = value;
+            }
+        }
+
+        private string _lastName;
+
+        /// <summary>
+        /// LastName of Admin
+        /// </summary>
+        [JsonProperty(PropertyName = "lastname")]
+        public string LastName
+        {
+            get
+            {
+                return this._lastName;
+            }
+            set
+            {
+                this._lastName = value;
+            }
+        }
+
+        private string _password;
+
+        /// <summary>
+        /// Password of Admin
+        /// </summary>
+        [JsonProperty(PropertyName = "password")]       
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                this._password = value;
+            }
+        }
+    }
+}
